7633bb91f6437f23f1d46504f1a37bfb92b2ee2e
Steve Brown <stephen.brown@konghq.com>
2025-05-11 16:10:31 +0100

    fix output

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.1] - 2025-05-11
+- Fixed SCIM outputs
+
 ## [1.0.0] - 2025-05-11
 - Added support SCIM
 
--- outputs.tf
+++ outputs.tf
@@ -86,7 +86,39 @@ output "kerberos_endpoint_alt" {
   value = jsondecode(data.http.metadata_alt.response_body).kerberos_endpoint
 }
 
+# SCIM-specific outputs (only if enabled)
 output "scim_enabled" {
   value = var.enable_scim
 }
 
+output "scim_configuration_instructions" {
+  value = (var.enable_scim ? <<-EOT
+    SCIM is enabled for this application. To complete configuration:
+    
+    1. Navigate to Azure Portal > Enterprise Applications > ${azuread_application.oidc.display_name}
+    2. Go to Provisioning tab
+    3. Click "Get started"
+    4. Set Provisioning Mode to "Automatic"
+    5. Configure Admin Credentials:
+       - Tenant URL: (Get from Insomnia SCIM panel)
+       - Secret Token: (Get from Insomnia SCIM panel)
+    6. Test Connection
+    7. Save the configuration
+    8. Go to Users and groups tab to assign users/groups
+    9. Start provisioning from the Provisioning tab
+    
+    Required Permissions Granted:
+    - User.ReadWrite.All
+    - Group.ReadWrite.All
+    - User.Export.All
+EOT
+  : "SCIM is not enabled for this application. Set enable_scim = true to enable SCIM support.")
+}
+
+output "service_principal_id" {
+  value = azuread_service_principal.oidc.id
+}
+
+output "app_roles" {
+  value = azuread_application.oidc.app_role
+}
